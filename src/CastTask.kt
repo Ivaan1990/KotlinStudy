7f4a19ca18e71fe7eedca5c6b0b4037efc75d602
Ivaan1990 <ivaan7845@gmail.com>
2019-08-13 15:22:18 +0300

    CastTask

---
+++ src/CastTask.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>){
+    var five: Char = '5'
+
+    var fiveStr = five.toString()
+
+    var byteStr = fiveStr.toByte()
+    var shortStr = fiveStr.toShort()
+    var intStr = fiveStr.toInt()
+    var doubleStr = fiveStr.toDouble()
+    var floatStr = fiveStr.toFloat()
+    var longStr = fiveStr.toLong()
+}
