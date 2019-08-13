f0d519e0506795b4e79f9fbc43878fc45cfdb83e
Ivaan1990 <ivaan7845@gmail.com>
2019-08-13 14:27:31 +0300

    comparison task

--- src/Comparison.kt
+++ src/Comparison.kt
@@ -1,3 +1,11 @@
 fun main(args: Array<String>){
+    print("Введите число 'a' ")
+    var a: Int = readLine().toString().toInt()
 
+    print("Введите число 'b' ")
+    var b: Int = readLine().toString().toInt()
+
+
+    var bool: Boolean = a > b
+    print(bool)
 }
