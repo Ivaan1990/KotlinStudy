5262093ff32d99bfa2e27d9b7d4a939aeea7efea
Ivaan1990 <ivaan7845@gmail.com>
2019-08-13 13:18:48 +0300

    commit

---
+++ src/Deposit.kt
@@ -0,0 +1,48 @@
+/*
+Напишите программу для расчета дохода по вкладу.
+Пользователь вводит сумму вклада, длительность вклада (количество месяцев) и ежемесячный процент по вкладу.
+Программа выводит в консоль информацию на сколько будет увеличиваться вклад каждый месяц.
+ */
+
+fun main(args: Array<String>){
+    println("Программа расчета вклада")
+
+    print("введите сумму вклада: ")
+    var sum: Int = input()
+
+    print("\nвведите срок (в месяцах): ")
+    var months: Int = inputMonth()
+
+    print("\nвведите желаемый процент по вкладу: ")
+    var percent: Int = input()
+
+    depositInfo(sum, months, percent)
+    montlyIncrease(sum, months, percent)
+}
+
+fun input(): Int {
+    return readLine().toString().toInt()
+}
+
+fun depositInfo(sum: Int, months: Int, percent: Int){
+    println("\nВы ввели следующие параметры по вкладу:\nсумма $sum\nсрок $months\nпроцент $percent")
+}
+
+fun inputMonth(): Int {
+    val error: String = "Введено недопустимое значение для срока вклада"
+    var months: Int = input()
+    if(months < 3) {
+        println(error)
+        months = 3
+    }
+    else if(months > 36) {
+        println(error)
+        months = 36
+    }
+    return months
+
+}
+
+fun montlyIncrease(sum: Int, months: Int, percent: Int){
+
+}
---
+++ src/TestGeography.kt
@@ -0,0 +1,74 @@
+fun main(args: Array<String>){
+    println("Тест на знание Географии")
+    val varOfAnswers = "Введите вариант ответа: "
+
+    println("Россия самая большая страна в мире по площади. Где расположена большая часть территории России?")
+    println("1. на востоке Европы и на севере Азии\n2. на юге \n3. на западе")
+    print(varOfAnswers)
+    var count: Int = 0
+    var answer: String = readLine().toString()
+    when(answer.toInt()){
+        1 -> {
+            println("Верно!")
+            count += 1
+        }
+        else -> println("Неверно")
+    }
+
+    println("Сколько часовых поясов в России?")
+    println("1. 9 часовых поясов\n2. 11 часовых поясов\n3. 4 часовых пояса")
+    print(varOfAnswers)
+    answer = readLine().toString()
+    when(answer.toInt()){
+        2 -> {
+            println("Верно!")
+            count += 1
+        }
+        else -> println("Неверно")
+    }
+
+    println("Сколько субъектов входит в состав РФ ?")
+    println("1. 50\n2. 10\n3. 85")
+    print(varOfAnswers)
+    answer = readLine().toString()
+    when(answer.toInt()){
+        3 -> {
+            println("Верно!")
+            count += 1
+        }
+        else -> println("Неверно")
+    }
+
+    println("Сколько стран граничит с Россией?")
+    println("1. 18\n2. 20\n3. 12")
+    print(varOfAnswers)
+    answer = readLine().toString()
+    when(answer.toInt()){
+        1 -> {
+            println("Верно!")
+            count += 1
+        }
+        else -> println("Неверно")
+    }
+
+    println("Сколько федеральных округов в России?")
+    println("1. 5\n2. 3\n3. 8")
+    print(varOfAnswers)
+    answer = readLine().toString()
+    when(answer.toInt()){
+        3 -> {
+            println("Верно!")
+            count += 1
+        }
+        else -> println("Неверно")
+    }
+
+    gameIsOverShowResult(count)
+}
+
+fun gameIsOverShowResult(count: Int){
+    when(count){
+        in 0..3 -> print("Ваши знания Географии России оцениваются на $count")
+        in 3..5 -> print("Отличный результат: $count")
+    }
+}
