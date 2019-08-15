0f9a99f07bb5107fc9243274c73c9186c2a10d19
Ivaan1990 <ivaan7845@gmail.com>
2019-08-15 12:49:21 +0300

    tasks 09

--- src/module_2/Task09.kt
+++ src/module_2/Task09.kt
@@ -1,7 +1,5 @@
 package module_2
 
-import java.lang.StringBuilder
-
 fun main(args: Array<String>){
     val table: Array<Array<String>> = Array(3, {Array(3,{""})})
     table[0] = arrayOf("Россия", "США", "Канада")
@@ -9,12 +7,15 @@ fun main(args: Array<String>){
     table[2] = arrayOf("Рубли", "Доллары", "Канадский доллар")
 
     println("Страна Столица Валюта\t")
+    var count = 0
+    var count2 = 0
     for(row in table){
         for(cell in row){
-            var elem: String = cell
+            var elem = table[count2++][count]
             print("$elem\t")
         }
+        count++
+        count2 = 0
         println()
     }
-
 }
