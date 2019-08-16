61ec2600fc1693cebee5b6fa9b818449f7913556
Ivaan1990 <ivaan7845@gmail.com>
2019-08-16 13:04:20 +0300

    tasks 10

--- src/module_2/Task10.kt
+++ src/module_2/Task10.kt
@@ -1,6 +1,62 @@
 package module_2
+/*
+Напишите программу для хранения названия городов. Пользователь может сделать 4 действия:
+- Добавить название города в программу.
+- Посмотреть в консоли список всех добавленных городов
+- Посмотреть список добавленных городов без повторений
+- Выход из программы
+Для хранения используйте коллекцию типа List.
+ */
+fun main(){
+    val towns = arrayListOf<String>()
+    loop@ while(true){
+        commandInfo()
+        val input: String = readLine().toString()
+        when(input.toInt()){
+            1 -> {
+                print("Введите название города: ")
+                val inputTown = readLine().toString()
+                towns.add(inputTown)
+            }
+            2 -> printAllTowns(towns)
+            3 -> printAllTownsWithoutRepetition(towns)
+            else -> {
+                break@loop
+            }
+        }
+    }
+}
 
-fun main(args: Array<String>){
-    var list: List<String> = listOf()
+/**
+ * Информация по доступным функциям программы
+ */
+fun commandInfo(){
+    println(
+        "Выберите команду:" +
+            "\n1 - Добавить название города в программу" +
+            "\n2 - Посмотреть в консоли список всех добавленных городов" +
+            "\n3 - Посмотреть список добавленных городов без повторений" +
+            "\n4 - Выход из программы"
+    )
+}
 
+/**
+ * Выводим все введённые города в консоль с повторами
+ */
+fun printAllTowns(towns: List<String>) {
+    println("Список всех добавленных городов:")
+    for(town in towns){
+        println(town)
+    }
+}
+
+/**
+ * Выводим все названия городов без повторов
+ */
+fun printAllTownsWithoutRepetition(names: List<String>){
+    println("Все введённые города без повторов:")
+    val setOfNames: Set<String> = names.toSet()
+    for(name in setOfNames){
+        println(name)
+    }
 }
